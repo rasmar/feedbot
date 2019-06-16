b0ce8a1a1eaf7c8bd4832549b1106039e8ec6ff4
Marcin Raszkiewicz <raszkiewicz.m@gmail.com>
2019-06-16 19:51:58 +0200

    Refactor in two lambdas

---
+++ source/handlers/slack_component.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative "../config/environment"
+
+module FeedBot
+  module Handler
+    module SlackComponent
+      extend self
+
+      def handle(event:, context:)
+        return { statusCode: 200, challenge: event["challenge"] } if event["challenge"]
+
+        event = Parsers::IncomingEvent.new(event).parse
+        event.process
+        { statusCode: 200 }
+      end
+    end
+  end
+end
