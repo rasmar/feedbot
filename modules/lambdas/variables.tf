549b2b95ea916c7ff458b698d29dd66517d8db31
Marcin Raszkiewicz <raszkiewicz.m@gmail.com>
2019-06-26 00:23:45 +0200

    Add variables

---
+++ modules/lambdas/variables.tf
@@ -0,0 +1,22 @@
+variable "slack_bot_token" {
+  type = "string"
+}
+variable "slack_oauth_token" {
+  type = "string"
+}
+variable "slack_leader_label_id" {
+  type = "string"
+}
+variable "salesforce_client_secret" {
+  type = "string"
+}
+variable "salesforce_client_key" {
+  type = "string"
+}
+variable "dynamodb_arn" {
+  type = "string"
+}
+variable "s3_bucket" {
+  type = "string"
+}
+
